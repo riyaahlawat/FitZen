3fc570c101b490e29294a0008540acd9616d572e
Payal Narwal <payal.narwal2003@gmail.com>
2024-10-04 14:49:10 +0530

    Update script-meditation-home.js

--- meditation/script-meditation-home.js
+++ meditation/script-meditation-home.js
@@ -4,52 +4,56 @@ let intervalId;
 function startAnimation() {
   if (!isAnimating) {
     isAnimating = true;
-    
+
     // Start ripple animation
-    document.querySelectorAll('.circle').forEach(circle => {
-      circle.style.animationPlayState = 'running';
+    document.querySelectorAll(".circle").forEach((circle) => {
+      circle.style.animationPlayState = "running";
     });
-    
+
     // Start the first music automatically
-    playMusic('relax.mp3');
-    
+    playMusic("relax.mp3");
+
     // Enable the stop button and disable the start button
-    document.getElementById('stopBtn').disabled = false;
-    document.getElementById('startBtn').disabled = true;
+    document.getElementById("stopBtn").disabled = false;
+    document.getElementById("startBtn").disabled = true;
   }
 }
 
 function stopAnimation() {
   if (isAnimating) {
     isAnimating = false;
-    
+
     // Stop ripple animation
-    document.querySelectorAll('.circle').forEach(circle => {
-      circle.style.animationPlayState = 'paused';
+    document.querySelectorAll(".circle").forEach((circle) => {
+      circle.style.animationPlayState = "paused";
     });
-    
+
     // Stop the audio
-    const audioPlayer = document.getElementById('meditation-audio');
+    const audioPlayer = document.getElementById("meditation-audio");
     audioPlayer.pause();
     audioPlayer.currentTime = 0; // Reset to start
-    
+
     // Disable the stop button and enable the start button
-    document.getElementById('stopBtn').disabled = true;
-    document.getElementById('startBtn').disabled = false;
+    document.getElementById("stopBtn").disabled = true;
+    document.getElementById("startBtn").disabled = false;
   }
 }
 
-function playMusic(musicFile) {
+function playMusic(musicFile, color) {
   // Play selected music
-  const audioPlayer = document.getElementById('meditation-audio');
-  audioPlayer.src = './assets/music/' + musicFile;
+  const audioPlayer = document.getElementById("meditation-audio");
+  audioPlayer.src = "./assets/music/" + musicFile;
   audioPlayer.play();
 
-  // Scroll to animation and start it
-  document.getElementById('circle-animation').scrollIntoView({ behavior: 'smooth' });
+  document
+    .getElementById("circle-animation")
+    .scrollIntoView({ behavior: "smooth" });
+
+  document.documentElement.style.setProperty("--animation-color", color);
+
   startAnimation();
 }
 
 // Attach event listeners to the start and stop buttons
-document.getElementById('startBtn').addEventListener('click', startAnimation);
-document.getElementById('stopBtn').addEventListener('click', stopAnimation);
+document.getElementById("startBtn").addEventListener("click", startAnimation);
+document.getElementById("stopBtn").addEventListener("click", stopAnimation);
